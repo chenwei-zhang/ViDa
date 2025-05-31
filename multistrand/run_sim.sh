7c8f52a69225c6837762221cba63b631a9e47bb4
Chenwei Zhang <49108806+chenwei-zhang@users.noreply.github.com>
2025-05-31 11:27:42 -0700

    clear multistrand codes

--- multistrand/run_sim.sh
+++ multistrand/run_sim.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# nohup python sample_trajs_machinek.py --row 0 --nsims 5000 --save_id 1e-7 --timestep 1e-7 > perfect_toehold7_reporter_1e-5.log 2>&1 &
-
-python sample_trajs_machinek.py --row 0 --nsims 10 --save_id 1e-7 --timestep 1e-7
-
-
-# nohup python sample_trajs_machinek.py --row 1 --nsims 2000 --save_id test_0 > proximal_toehold7_reporter_test_0.log 2>&1 &
+python sample_trajs_machinek.py --row 0 --nsims 10 --save_id test --timestep 1e-7
--- multistrand/v0/run.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-
-
-parallel -j 32 python3 Machinek_trajmode_parallel.py ::: {161..199}
-echo "All processes have completed."
