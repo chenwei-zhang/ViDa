1b183352f07306b6ced32f88f4f7a1d56820973b
Chenwei Zhang <49108806+chenwei-zhang@users.noreply.github.com>
2025-08-04 02:27:56 -0700

    rearranged data directory and codes

--- vida/data_processing/read_data.py
+++ vida/data_processing/read_data.py
@@ -18,12 +18,18 @@ def main():
     reaction_id = args.rxn
     num_traj = args.num_traj
 
-    inpath = "data/raw_data"
-    outpath = "data/post_data/{}/{}.pkl.gz".format(reaction_id, reaction_id)
-
-    sequences = load_raw_data("data/raw_data_gao.csv", reaction_id)
-       
-    base_names = assign_base_names(sequences['P'], sequences['T'])
+    if 'gao' in reaction_id.lower():
+        inpath = "data/raw_data/gao_data"
+        outpath = "data/post_data/{}/{}.pkl.gz".format(reaction_id, reaction_id)
+        sequences = load_raw_data("data/raw_data_gao.csv", reaction_id)
+        base_names = assign_base_names(sequences['P'], sequences['T'])
+        
+    elif 'machinek' in reaction_id.lower():
+        inpath = "data/raw_data/machinek_data"
+        outpath = "data/post_data/{}/{}.pkl.gz".format(reaction_id, reaction_id)
+        sequences = load_raw_data("data/raw_data_machinek.csv", reaction_id)
+        base_names = assign_base_names(sequences['incumbent'], sequences['substrate'], sequences['invader'])
+     
     
     # Load data
     fpath = os.path.join(inpath, f"{reaction_id}.hdf5")
